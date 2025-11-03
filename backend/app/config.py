14ccd36d5ab1b64a2eaa95e183d4f5c7dcfe4493
divyakumar.p <divyakumar.p@williamoneilindia.com>
2025-11-03 12:17:02 +0530

    reverted changes

--- backend/app/config.py
+++ backend/app/config.py
@@ -31,8 +31,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
-        case_sensitive=False,
-        extra="ignore"  # Ignore extra fields from .env file
+        case_sensitive=False
     )
 
 
